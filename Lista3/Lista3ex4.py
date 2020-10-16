320a036056985498cca6e834758332d6eb967bbe
Hugo Paiva <52087730+hugo-paiva@users.noreply.github.com>
2020-10-16 02:40:23 -0300

    Update Lista3ex4.py

--- Lista3/Lista3ex4.py
+++ Lista3/Lista3ex4.py
@@ -1,5 +1,6 @@
 alunos = int(input())
 cont_aprovados = soma_geral = 0
+
 for aluno in range(alunos):
     a, b, c, faltas = input().split()
     a, b, c, faltas = [float(a), float(b), float(c), int(faltas)]
@@ -8,10 +9,11 @@ for aluno in range(alunos):
     if media >= 5 and faltas <= 16:
         print('Aprovado')
         cont_aprovados += 1
-    elif media >= 3 and faltas <= 16:
+    elif media > 3 and faltas <= 16:
         print('Exame')
     else:
         print('Reprovado')
     soma_geral += media
+
 media_turma = soma_geral / alunos
 print(f'{media_turma:.1f} {cont_aprovados}')
