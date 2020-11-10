7e26aafd8b783c65df5fffe052d3c4ff8ceff118
Hugo Paiva <52087730+hugo-paiva@users.noreply.github.com>
2020-11-10 03:51:11 -0300

    aprimorando ex 7

--- Lista6/Lista6ex7_iterativo_Aprimorado.py
+++ Lista6/Lista6ex7_iterativo_Aprimorado.py
@@ -1,16 +1,8 @@
-def vazio(pino):
-    if pino == []:
-        return True
-    return False
-
-
 def pode_mover(x, y):
-    if vazio(y) or not vazio(x) and x[-1] < y[-1]:
+    if y == [] or x != [] and x[-1] < y[-1]:
         y.append(x.pop())
         return True
 
-
-cont = 0
 def verificar_que_terminou(passos):
     global cont, discos
     cont += 1
@@ -18,35 +10,36 @@ def verificar_que_terminou(passos):
         return True
     return False
 
-
-def hanoi_iterativo(discos, A, B, C, passos):
-    if len(A) % 2 == 0:
+def hanoi_iterativo(discos, a, b, c, passos):
+    global cont
+    cont = 0
+    if len(a) % 2 == 0:
         while True:
 
-            if pode_mover(A, B) or pode_mover(B, A): # Tentamos o movimento em ambas direções
+            if pode_mover(a, b) or pode_mover(b, a): # Tentamos o movimento em ambas direções
                 if verificar_que_terminou(passos): return
 
-            if pode_mover(A, C) or pode_mover(C, A): # Tentamos o movimento em ambas direções
+            if pode_mover(a, c) or pode_mover(c, a): # Tentamos o movimento em ambas direções
                 if verificar_que_terminou(passos): return
 
-            if pode_mover(B, C) or pode_mover(C, B): # Tentamos o movimento em ambas direções
+            if pode_mover(b, c) or pode_mover(c, b): # Tentamos o movimento em ambas direções
                 if verificar_que_terminou(passos): return
 
     else:
         while True:
 
-            if pode_mover(A, C) or pode_mover(C, A): # Tentamos o movimento em ambas direções
+            if pode_mover(a, c) or pode_mover(c, a): # Tentamos o movimento em ambas direções
                 if verificar_que_terminou(passos): return
 
-            if pode_mover(A, B) or pode_mover(B, A): # Tentamos o movimento em ambas direções
+            if pode_mover(a, b) or pode_mover(b, a): # Tentamos o movimento em ambas direções
                 if verificar_que_terminou(passos): return
 
-            if pode_mover(B, C) or pode_mover(C, B): # Tentamos o movimento em ambas direções
+            if pode_mover(b, c) or pode_mover(c, b): # Tentamos o movimento em ambas direções
                 if verificar_que_terminou(passos): return
-
 
 discos, passos = input().split()
 discos, passos = int(discos), int(passos)
+
 A, B, C = list(range(discos, 0, -1)), [], []
 hanoi_iterativo(discos, A, B, C, passos)
 print(len(A), len(B), len(C), sep=' ')
